879149ea86c51af248fb8811025967c4008661dc
bytewizard <superjump2p@gmail.com>
2026-09-18 19:03:05 +0300

    update menu buttons

--- keyboards/user_kb.py
+++ keyboards/user_kb.py
@@ -7,6 +7,7 @@ from config import config
 
 
 def main_menu_kb(is_admin: bool = False) -> ReplyKeyboardMarkup:
+    if not is_admin:
     builder = ReplyKeyboardBuilder()
     builder.row(KeyboardButton(text="📅 Записаться"))
     builder.row(KeyboardButton(text="💅 Прайсы"), KeyboardButton(text="🖼 Портфолио"))
